a560fdb94eb259e6bb90fc7305c178b927f34d41
Abhijeet Wadkar <Abhijeet@Abhijeets-MacBook-Pro.local>
2016-12-14 23:28:06 -0500

    added model

--- main.lua
+++ main.lua
@@ -1,14 +1,15 @@
 
 require 'torch'
 require 'nn'
--- require 'dpnn'
--- require 'hdf5'
 require 'optim'
 require 'utils.DataLoader'
+require 'models.vgg'
+-- require 'dpnn'
+-- require 'hdf5'
+
 local opts = require('utils.parseopt')
 
 local function main()
-  
   -- parse command line input
   local opt = opts.parse(arg)
   
@@ -17,8 +18,25 @@ local function main()
   collectgarbage()
   print('Data loaded in memory...')
   
-  imgs, labels  = data_loader:next_batch()
+  -- creating vggnet model
+  model = create_vgg_model(1000)
+  print(model)
   
+  -- define criteria
+  criterion = nn.ClassNLLCriterion()
+  
+  batch_size = opt.batch_size
+  number_iterations = opt.iterations
+  num_batches = math.floor(opt.num_training/opt.batch_size)
+  for i = 1, number_iterations*num_batches do
+    imgs, labels  = data_loader:next_batch()
+    model:zeroGradParameters()
+    output = model:forward(imgs)
+    loss = criterion:forward(output, labels)
+    gradient = criterion:backward(output, labels)
+    model:backward(imgs, gradient)
+    print("Loss: " ..loss)
+  end
     
 end
 
